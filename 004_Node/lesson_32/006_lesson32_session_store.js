5e3b3977ed44fbc101929ed1685cd25c907f2209
NotMe1994 <moggeth@yandex.ru>
2016-08-17 12:33:48 +0300

    update

--- 004_Node/lesson_32/006_lesson32_session_store.js
+++ 004_Node/lesson_32/006_lesson32_session_store.js
@@ -1,31 +1,29 @@
-п»їvar express = require('express');
+var express = require('express');
 var app = express();
 
-var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
-// РїРѕРґРєР»СЋС‡РµРЅРёРµ РјРѕРґСѓР»СЏ express-mysql-session 
+// подключение модуля express-mysql-session 
 var MySQLStore = require('express-mysql-session')(session);
 
 
 var options = {
-    // РїР°СЂР°РјРµС‚СЂС‹ СЃРѕРµРґРёРЅРµРЅРёСЏ СЃ Р±Рґ 
+    // параметры соединения с бд 
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: '',
     database: 'session_test',
 
-    // РєР°Рє С‡Р°СЃС‚Рѕ Р±СѓРґРµС‚ РїСЂРѕРІРѕРґРёС‚СЊСЃСЏ СѓРґР°Р»РµРЅРёРµ СѓСЃС‚Р°СЂРµРІС€РёС… СЃРµСЃСЃРёР№(РјРёР»Р»РёСЃРµРєСѓРЅРґС‹)
+    // как часто будет проводиться удаление устаревших сессий(миллисекунды)
     checkExpirationInterval: 900000,
-    // РІСЂРµРјСЏ СѓСЃС‚Р°СЂРµРІР°РЅРёСЏ СЃРµСЃСЃРёРё(РјРёР»Р»РёСЃРµРєСѓРЅРґС‹)
+    // время устаревания сессии(миллисекунды)
     expiration: 86400000
 };
 
-// СЃРѕР·РґР°РЅРёРµ С…СЂР°РЅРёР»РёС‰Р° РґР»СЏ СЃРµСЃСЃРёРё 
+// создание хранилища для сессии 
 var sessionStore = new MySQLStore(options);
 
-app.use(cookieParser());
 app.use(session({
  
     secret: 'secret',
