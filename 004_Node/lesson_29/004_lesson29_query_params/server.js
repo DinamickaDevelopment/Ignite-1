8fd71a88a895358e7564084b03640fef1c51816f
NotMe1994 <moggeth@yandex.ru>
2016-09-23 01:37:11 +0300

    update

--- 004_Node/lesson_29/004_lesson29_query_params/server.js
+++ 004_Node/lesson_29/004_lesson29_query_params/server.js
@@ -5,7 +5,15 @@ var port = process.env.port || 1337;
 
 http.createServer(function (req, res) {
 
-    // доступ к query параметрам
+    // Для получения доступа к query параметрам используется метод url.parse 
+	// синтаксис: url.parse(urlString[, parseQueryString]) 
+	
+	// urlString - строковое представление URL 
+	// parseQueryString - boolean. Если значение true, query параметры будут представлены в виде JS объекта.
+	
+	// Таким образом, при парсинге строки 'some/url?param=1' query параметры будут представлены объектом { param: 1 } и доступны через свойство query
+	
+	// Если значение второго параметра url.parse - false, query параметры будут представлены в виде строки. 
     var query = url.parse(req.url, true).query;
     res.end('get params: ' + JSON.stringify(query));
 
